ef8df66ff4937440787e7562841a74b6fcb4c819
Emanuele Vitória <148836387+Manu-Vii@users.noreply.github.com>
2025-09-05 15:39:22 -0300

    Delete

--- WePayU/src/br/ufal/ic/p2/wepayu/Exception/NomeInvalidoException.java
+++
@@ -1,7 +0,0 @@
-package br.ufal.ic.p2.wepayu.Exception;
-
-public class NomeInvalidoException extends Exception {
-    public NomeInvalidoException() {
-        super("Nome deve conter apenas letras e espacos.");
-    }
-}
--- WePayU/src/br/ufal/ic/p2/wepayu/managers/MainManager.java
+++
@@ -1,9 +0,0 @@
-package br.ufal.ic.p2.wepayu.managers;
-
-public class MainManager {
-    public EmpregadoManager empregadoManager = new EmpregadoManager();
-
-    public EmpregadoManager getEmpregadoManager() {
-        return empregadoManager;
-    }
-}
